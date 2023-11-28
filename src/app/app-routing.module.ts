a0d03dd7b61a63a298e37ff4a087e888e044b22f
fad-anes <anes.fad@esprit.tn>
2023-11-28 15:11:52 +0100

    Integration des templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomefrontComponent } from './homefront/homefront.component';
+import { AdminComponent } from './admin/admin.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomefrontComponent , title:"Acceuil" },
+  { path: 'Admin', component: AdminComponent , title:"Administrateur" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderFrontComponent } from './header-front/header-front.component';
+import { HomefrontComponent } from './homefront/homefront.component';
+import { FooterFrontComponent } from './footer-front/footer-front.component';
+import { FooterBackComponent } from './footer-back/footer-back.component';
+import { HeaderBackComponent } from './header-back/header-back.component';
+import { AdminComponent } from './admin/admin.component';
+import { MenuadminComponent } from './menuadmin/menuadmin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderFrontComponent,
+    HomefrontComponent,
+    FooterFrontComponent,
+    FooterBackComponent,
+    HeaderBackComponent,
+    AdminComponent,
+    MenuadminComponent
   ],
   imports: [
     BrowserModule,
