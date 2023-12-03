854128d5e3b99a8710c893b5eea7a4bc8628bb02
fad-anes <anes.fad@esprit.tn>
2023-12-03 12:46:41 +0100

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomefrontComponent } from './homefront/homefront.component';
 import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
   { path: 'Home', component: HomefrontComponent , title:"Acceuil" },
   { path: 'Admin', component: AdminComponent , title:"Administrateur" },
+  { path: 'login', component: LoginComponent , title:"Se Connecter" },
+  { path: 'register', component: RegisterComponent , title:"Creer un compte" },
 ];
 
 @NgModule({
